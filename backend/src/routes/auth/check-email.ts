25bb0592f1ecf97978b587ec1b504971fe860f79
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-12-04 12:09:31 +0100

    feat: add check email route

---
+++ backend/src/routes/auth/check-email.ts
@@ -0,0 +1,33 @@
+import { Router, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { readUsers } from "../../utils/fileStorage";
+
+const router = Router();
+
+// POST api/auth/check-email
+router.post(
+  "/",
+  [body("email").isEmail()],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email } = req.body;
+    const users = readUsers();
+    const existingEmail = users.find((u) => u.email === email);
+
+    if (existingEmail) {
+      return res
+        .status(409)
+        .json({ message: "Taki email już istnieje", available: false });
+    }
+
+    res.json({
+      available: true,
+    });
+  }
+);
+
+export default router;
