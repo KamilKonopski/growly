318848a63cb8d6b028708cddf6bfa3427d62d57e
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-12-04 14:33:36 +0100

    feat: add token auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import AppRoutes from "./routes/AppRoutes";
+import { setUser, setMode } from "./store/appSlice";
+import { jwtDecode } from "jwt-decode";
+
+interface JwtUser {
+  id: string;
+  name: string;
+  email: string;
+  iat: number;
+  exp: number;
+}
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decoded = jwtDecode<JwtUser>(token);
+        dispatch(
+          setUser({ id: decoded.id, name: decoded.name, email: decoded.email })
+        );
+        dispatch(setMode("backend"));
+      } catch (err) {
+        console.error("Błędny token w localStorage", err);
+        localStorage.removeItem("token");
+      }
+    }
+  }, [dispatch]);
+
   return <AppRoutes />;
 };
 
