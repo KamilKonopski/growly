a751d328430708178d059c589838652123f21f0b
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-12-09 14:09:43 +0100

    feat: add habits routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import authRoutes from "./auth/index";
+import habitRoutes from "./habits/index";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/habits", habitRoutes);
 
 router.get("/", (req, res) => {
   res.json({ message: "Growly API is running..." });
