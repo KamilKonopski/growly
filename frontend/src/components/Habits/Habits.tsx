5550f3b421f50c70c127ae5135baacb497f7d8d3
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2025-12-12 11:00:34 +0100

    feat: displaying a list of habits and a single habit card, adding the option to uncheck a completed habit

--- frontend/src/components/Habits/Habits.tsx
+++ frontend/src/components/Habits/Habits.tsx
@@ -1,5 +1,27 @@
+import { Plus } from "lucide-react";
+
+import HabitList from "./HabitList/HabitList";
+
+import styles from "./Habits.module.css";
+
 const Habits = () => {
-  return <div>Habits</div>;
+  return (
+    <section className={styles.container}>
+      <div className={styles["button_container"]}>
+        <button className={styles.button}>
+          <Plus />
+          Dodaj nawyk
+        </button>
+      </div>
+      <HabitList />
+      <div>
+        <p>Kalendarz</p>
+      </div>
+      <div>
+        <p>Statystyki</p>
+      </div>
+    </section>
+  );
 };
 
 export default Habits;
