56eae1ae193ca51fdd7d0d80d4514947ded11c85
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2026-01-27 13:40:50 +0100

    feat: add stats, settings, profile routes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 import { useSelector } from "react-redux";
+
 import type { RootState } from "../store/store";
 
 import Authentication from "../components/Authentication/Authentication";
@@ -10,6 +10,9 @@ import Habits from "../components/Habits/Habits";
 import Learning from "../components/Learning/Learning";
 import MainLayout from "../components/Layout/MainLayout";
 import NotFound from "../components/NotFound/NotFound";
+import Statistics from "../components/Statistics/Statistics";
+import Settings from "../components/Settings/Settings";
+import UserProfile from "../components/UserProfile/UserProfile";
 
 const AppRoutes = () => {
   const user = useSelector((state: RootState) => state.app.user);
@@ -42,6 +45,9 @@ const AppRoutes = () => {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/habits" element={<Habits />} />
           <Route path="/learning" element={<Learning />} />
+          <Route path="/stats" element={<Statistics />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<UserProfile />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
