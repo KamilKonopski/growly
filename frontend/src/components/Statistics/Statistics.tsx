82ece812bd8c326abfd0d87d9063b513bcbb0ed2
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2026-01-27 13:41:29 +0100

    feat: create components to future development

---
+++ frontend/src/components/Statistics/Statistics.tsx
@@ -0,0 +1,11 @@
+import EmptyState from "../../common/components/EmptyState/EmptyState";
+
+const Statistics = () => {
+  return (
+    <EmptyState>
+      <p>Widok statystyk w budowie :)</p>
+    </EmptyState>
+  );
+};
+
+export default Statistics;
---
+++ frontend/src/components/UserProfile/UserProfile.tsx
@@ -0,0 +1,11 @@
+import EmptyState from "../../common/components/EmptyState/EmptyState";
+
+const UserProfile = () => {
+  return (
+    <EmptyState>
+      <p>Widok profilu użytkownika w budowie :)</p>
+    </EmptyState>
+  );
+};
+
+export default UserProfile;
