d10dc9d8ebf1de8b0c796df0b5c6e35dd85fb180
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2026-01-31 20:27:39 +0100

    feat: add simple settings component

--- frontend/src/components/Settings/Settings.tsx
+++ frontend/src/components/Settings/Settings.tsx
@@ -1,10 +1,20 @@
-import EmptyState from "../../common/components/EmptyState/EmptyState";
+import styles from "./Settings.module.css";
 
 const Settings = () => {
   return (
-    <EmptyState>
-      <p>Widok ustawień w budowie :)</p>
-    </EmptyState>
+    <section className={styles.container}>
+      <div className={styles["settings-container"]}>
+        <div className={styles["info-container"]}>
+          <p>
+            Autor:{" "}
+            <a href="https://github.com/kamilkonopski" target="_blank">
+              Kamil Konopski
+            </a>
+          </p>
+          <span className={styles.version}>v0.1.0</span>
+        </div>
+      </div>
+    </section>
   );
 };
 
