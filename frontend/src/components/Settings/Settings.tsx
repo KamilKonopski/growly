2f896b6d727aaf67bd1dcb30abda549875ab48a7
KamilKonopski <Kamil.Konopski.2000@gmail.com>
2026-02-11 18:57:25 +0100

    feat: add mode info & change app version

--- frontend/src/components/Settings/Settings.tsx
+++ frontend/src/components/Settings/Settings.tsx
@@ -1,10 +1,15 @@
 import { motion } from "framer-motion";
 
+import { useSelector } from "react-redux";
+import type { RootState } from "../../store/store";
+
 import { componentMountVariants } from "../../common/config/config";
 
 import styles from "./Settings.module.css";
 
 const Settings = () => {
+  const mode = useSelector((state: RootState) => state.app.mode);
+
   return (
     <motion.section
       variants={componentMountVariants}
@@ -13,6 +18,9 @@ const Settings = () => {
       className={styles.container}
     >
       <div className={styles["settings-container"]}>
+        <div className={styles.mode}>
+          <p>Tryb aplikacji: {mode === "backend" ? "local backend" : "demo"}</p>
+        </div>
         <div className={styles["info-container"]}>
           <p>
             Autor:{" "}
@@ -20,7 +28,7 @@ const Settings = () => {
               Kamil Konopski
             </a>
           </p>
-          <span className={styles.version}>v0.1.0</span>
+          <span className={styles.version}>v1.0.0</span>
         </div>
       </div>
     </motion.section>
